022d91412f607e7fa3f0bf867a9b25422a9a34d1
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-08-18 17:30:27 +0530

    Hello World

---
+++ Basic-Deployment/public_html/HelloWorld.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello World PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="container">
+          <h1>Test PHP</h1>
+
+          <div class="code-content">
+
+            <?php
+
+              echo "Hello World!"
+
+            ?>
+            
+          </div>
+    </div>
+
+  </body>
+</html>
