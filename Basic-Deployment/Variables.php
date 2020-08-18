e51feb2db2725a4a317efdf00f7318fa1536418e
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-08-18 17:57:29 +0530

    Variables

---
+++ Basic-Deployment/Variables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Variables PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="container">
+          <h1>Variables</h1>
+
+          <div class="code-content">
+
+            <?php
+
+                $name = "Nishant";
+                $number = 2;
+
+                echo $name."<br/>".$number;
+            ?>
+            
+          </div>
+    </div>
+
+  </body>
+</html>
