45963d8ef9d683a7a175d1cc64fd7344c1f177b9
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-08-21 14:02:08 +0530

    HTML in PHP

---
+++ Basic-Deployment/html.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP and HTML</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="container">
+          <h1>PHP in HTML and HTML in PHP</h1>
+
+          <div class="code-content">
+
+            <h1><?php echo "Hello Nishant!" ?></h1>
+
+            <?php echo "<h1>Hello Nishant!</h1>" ?>
+            
+          </div>
+    </div>
+
+  </body>
+</html>
